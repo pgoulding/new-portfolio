d8ca38ced17a01cbfbb75d6c7fb4c0d112cebcd0
Patrick Goulding <patrickgoulding@gmail.com>
2022-11-14 16:08:22 -0800

    fixed indents and spaces

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -41,27 +41,27 @@ class Resume extends Component {
       <section id="resume">
 
       <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
+        <div className="three columns header-col">
+          <h1><span>Education</span></h1>
+        </div>
 
-         <div className="nine columns main-col">
+        <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">
+                {education}
+              </div>
             </div>
-         </div>
+        </div>
       </div>
 
 
       <div className="row work">
 
-         <div className="three columns header-col">
+        <div className="three columns header-col">
             <h1><span>Work</span></h1>
-         </div>
+        </div>
 
-         <div className="nine columns main-col">
+        <div className="nine columns main-col">
           {work}
         </div>
     </div>
@@ -70,23 +70,23 @@ class Resume extends Component {
 
       <div className="row skill">
 
-         <div className="three columns header-col">
+        <div className="three columns header-col">
             <h1><span>Skills</span></h1>
-         </div>
+        </div>
 
-         <div className="nine columns main-col">
+        <div className="nine columns main-col">
 
             <p>{skillmessage}
             </p>
 
 				<div className="bars">
-				   <ul className="skills">
-					  {skills}
+          <ul className="skills">
+            {skills}
 					</ul>
 				</div>
 			</div>
       </div>
-   </section>
+  </section>
     );
   }
 }
